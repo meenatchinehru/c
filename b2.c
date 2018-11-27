a1cb8d1cd4255ed13bd578303a43fea7b9145aca
meenatchinehru <45060606+meenatchinehru@users.noreply.github.com>
2018-11-27 13:21:57 +0530

    Create b2.c

---
+++ b2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int n;
+   clrscr();
+   printf("n=");
+   scanf("%d",&n);
+   if(n%2==0)
+   {
+      printf("even");
+   }
+   else if(n%2==1)
+   {
+            printf("odd");
+    }
+    getch();
+   }
